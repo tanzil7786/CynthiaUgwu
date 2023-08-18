8caa8ecb76912dd85c0481b472e73041b5589c1b
tanzil7786 <tanzilpasha1@gmail.com>
2023-08-18 21:21:25 +0530

    final changes

---
+++ script.js
@@ -0,0 +1,112 @@
+var timeout;
+
+const scroll = new LocomotiveScroll({
+  el: document.querySelector("#main"),
+  smooth: true,
+});
+
+function firstPageAnim() {
+  var tl = gsap.timeline();
+  tl.from("#nav", {
+    y: -10,
+    opacity: 0,
+    duration: 1.5,
+    ease: Expo.easeInOut,
+  })
+    .to(".boundingElem", {
+      y: 0,
+      duration: 2,
+      ease: Expo.easeInOut,
+      stagger: 0.2,
+      delay: -1,
+    })
+    .from("#end", {
+      y: -10,
+      duration: 1.5,
+      opacity: 0,
+      delay: -1,
+      ease: Expo.easeInOut,
+    });
+}
+
+function circleSkew() {
+  // default scale value
+  var xscale = 1;
+  var yscale = 1;
+
+  var xprev = 0;
+  var yprev = 0;
+
+  window.addEventListener("mousemove", function (dets) {
+    clearTimeout(timeout);
+
+    xscale = gsap.utils.clamp(0.8, 1.2, dets.clientX - xprev);
+    yscale = gsap.utils.clamp(0.8, 1.2, dets.clientY - yprev);
+
+    xprev = dets.clientX;
+    yprev = dets.clientY;
+
+    circleMouseFollower(xscale, yscale);
+
+    timeout = setTimeout(function () {
+      document.querySelector(
+        "#minicircle"
+      ).style.transform = `translate(${dets.clientX}px, ${dets.clientY}px) scale(1, 1)`;
+    }, 100);
+  });
+}
+
+function circleMouseFollower(xscale, yscale) {
+  window.addEventListener("mousemove", function (dets) {
+    document.querySelector(
+      "#minicircle"
+    ).style.transform = `translate(${dets.clientX}px, ${dets.clientY}px) scale(${xscale}, ${yscale})`;
+  });
+}
+
+circleSkew();
+circleMouseFollower();
+firstPageAnim();
+
+document.querySelectorAll(".elem").forEach(function (elem) {
+  var rotate = 0;
+  var diffrot = 0;
+
+  elem.addEventListener("mouseleave", function (dets) {
+    gsap.to(elem.querySelector("img"), {
+      opacity: 0,
+      ease: Power3,
+      duration: 0.5,
+    });
+  });
+
+  elem.addEventListener("mousemove", function (dets) {
+    var diff = dets.clientY - elem.getBoundingClientRect().top;
+    diffrot = dets.clientX - rotate;
+    rotate = dets.clientX;
+    gsap.to(elem.querySelector("img"), {
+      opacity: 1,
+      ease: Power3,
+      top: diff,
+      left: dets.clientX,
+      rotate: gsap.utils.clamp(-20, 20, diffrot * 0.5),
+    });
+  });
+});
+
+let time = document.getElementById("time");
+function displayTime() {
+  const now = new Date();
+  let hour = now.getHours();
+  const min = now.getMinutes();
+  const sec = now.getSeconds(); // Corrected this line
+  let am_pm = "AM";
+  if (hour > 12) {
+    am_pm = "PM";
+    hour = hour - 12;
+  }
+  time.textContent = `${hour}:${min}:${sec} ${am_pm}`; // Set the content of the "time" element
+
+  setTimeout(displayTime, 1000); // Update the time every second
+}
+displayTime();
